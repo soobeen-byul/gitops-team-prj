36ce0ee55ac820209251142f44367505980cfcfa
bksooon <sbka0704@nate.com>
2023-08-03 17:38:42 +0900

    bkson controller add

---
+++ src/main/java/com/example/demo/bksonController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class subController {
+    private final ResumeService resumeService;
+
+    @GetMapping("/resume/bkson")
+    public ResponseEntity<Resume> createSub() {
+        String name="손병규";
+        String github="github.com/bksooon";
+        String aboutme="안녕하세요. 손병규입니다.";
+
+        Resume subResume = new Resume(name,github,aboutme);
+
+        resumeService.create(name,github,aboutme);
+        
+        return ResponseEntity.ok()
+                .body(bksonResume);
+    }
+}
